00e1aef432b7fbfe66179edf6f3b2c1a36525e2d
Vladimir Berezenko <qmaster2000@gmail.com>
2023-03-27 17:20:12 +0300

    Fix build system to newer one

---
+++ asyncframework/__init__.py
@@ -0,0 +1,15 @@
+# -*- coding:utf-8 -*-
+
+__version__ = '0.1.4'
+
+__title__ = 'asyncframework'
+__description__ = 'Async framework easy async python applications and serivces development library.'
+__url__ = 'https://github.com/Q-Master/framework.py'
+__uri__ = __url__
+__doc__ = f"{__description__} <{__uri__}>"
+
+__author__ = 'Vladimir Berezenko'
+__email__ = 'qmaster2000@gmail.com'
+
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2019-2023 Vladimir Berezenko'
