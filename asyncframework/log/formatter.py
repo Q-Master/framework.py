460f04b888c00ac1352ccd1b9191c5c1b5c66e57
Vladimir Berezenko <qmaster2000@gmail.com>
2023-04-14 18:37:32 +0300

    Added adapter to be able to tag logs

--- asyncframework/log/formatter.py
+++ asyncframework/log/formatter.py
@@ -1,8 +1,10 @@
 # -*- coding:utf-8 -*-
+from typing import Tuple
+from itertools import chain
 import logging
 
 
-__all__ = ['LogFormatter']
+__all__ = ['LogFormatter', 'LoggerTaggingAdapter']
 
 
 class LogFormatter(logging.Formatter):
@@ -18,3 +20,24 @@ class LogFormatter(logging.Formatter):
         else:
             record.tags = '{' + ','.join(('='.join(map(str, i)) for i in record.tags.items())) + '}'
         return super().format(record)
+
+
+class LoggerTaggingAdapter(logging.LoggerAdapter):
+    tags: dict = {}
+    extra: dict = {}
+
+    def __init__(self, logger, extra = None) -> None:
+        if isinstance(logger, LoggerTaggingAdapter):
+            logger, self.tags = logger.logger, logger.tags.copy()
+        else:
+            pass
+        super().__init__(logger, extra if extra is not None else {})
+        self.extra['tags'] = self.tags
+        
+    def process(self, msg, kwargs) -> Tuple[str, dict]:
+        extra: dict = kwargs.setdefault('extra', self.extra)
+        if extra is not self.extra:
+            tags: dict = extra.setdefault('tags', self.tags)
+            if tags is not self.tags:
+                extra['tags'] = dict(chain(tags.items(), self.tags.items()))
+        return msg, kwargs
