3a2573975556e6a1588ddf7ce84e797c508481ec
Vladimir Berezenko <qmaster2000@gmail.com>
2023-05-22 00:29:36 +0300

    Fix: removed PacketWithId dependency.

--- asyncframework/__init__.py
+++ asyncframework/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding:utf-8 -*-
 
-__version__ = '0.1.17'
+__version__ = '0.1.18'
 
 __title__ = 'asyncframework'
 __description__ = 'Async framework easy async python applications and serivces development library.'
--- asyncframework/rpc/packets/rpc.py
+++ asyncframework/rpc/packets/rpc.py
@@ -2,7 +2,7 @@
 import asyncio
 from typing import Any, Sequence, Optional, Dict, Tuple, Callable, TypeVar, Union
 from logging import Logger
-from packets import PacketWithID
+from packets import Packet
 from ..decorator import rpc_methods
 from ..rpc import RPC
 from ..types import RPCException, Request, Response, WrongConsumer
@@ -18,13 +18,13 @@ T = TypeVar('T')
 
 class RPCPackets(RPC[T]):
     log: Logger = get_logger('RPCPackets')
-    response_models: Dict[int, PacketWithID] = {}
+    response_models: Dict[int, Packet] = {}
 
     def __init__(self, 
         app: T, 
         connection: ConnectionBase, 
         *args, 
-        response_models: Optional[Sequence[PacketWithID]] = None, 
+        response_models: Optional[Sequence[Packet]] = None, 
         methods: Dict[str, Tuple[Callable, Any]] = rpc_methods, 
         dont_receive=False, 
         **kwargs
@@ -36,19 +36,20 @@ class RPCPackets(RPC[T]):
             connection (ConnectionBase): the rpc connection
             raise_on_unregistered (bool, optional): raise error if the id is not registered. Defaults to True.
             methods (Dict[str, Tuple[Callable, Any]], optional): mapping of id: method used to dispatch the rpc calls. Defaults to rpc_methods.
-            response_models (Optional[Sequence[PacketWithID]], optional): `packet.PacketWithID` types for response deserializing. Defaults to None.
+            response_models (Optional[Sequence[Packet]], optional): `packet.Packet` types for response deserializing. Defaults to None.
             dont_receive (bool, optional): dont receive anything. Defaults to False.        
         """
         super().__init__(app, connection, *args, methods=methods, dont_receive=dont_receive, **kwargs)
         if response_models:
             for packet in response_models:
-                self.response_models[packet.__packet_id__] = packet
+                if 'packet_id' in packet.fields_names():
+                    self.response_models[packet.packet_id] = packet
         else:
             self.response_models = {}
 
     async def call(
             self,
-            request: PacketWithID,
+            request: Packet,
             *args,
             response_required: bool = True,
             correlation_id: Union[None, str] = None,
@@ -58,7 +59,7 @@ class RPCPackets(RPC[T]):
         """Call the remote method
 
         Args:
-            request (PacketWithID): _description_
+            request (Packet): _description_
             response_required (bool, optional): waiting for response or not. Defaults to True.
             correlation_id (Union[None, str], optional): id for request (auto if None). Defaults to None.
             wait_timeout (Union[None, int, float], optional): timeout to wait for response. Defaults to None.
@@ -82,9 +83,12 @@ class RPCPackets(RPC[T]):
 
             packet_cls = None
             if isinstance(response.result, dict) and '_' in response.result:
-                packet_id: int = response.result['_']
-                packet_cls = self.response_models.get(packet_id, None)
-
+                packet_id: Optional[int] = response.result.get('_', None)
+                if packet_id is not None:
+                    packet_cls = self.response_models.get(packet_id, None)
+                else:
+                    packet_cls = None
+            
             if packet_cls:
                 packet = packet_cls.load(response.result)
                 future.set_result(packet)
@@ -97,18 +101,19 @@ class RPCPackets(RPC[T]):
         if not isinstance(request.message, dict) or '_' not in request.message:
             raise Exception(f'No packet id in request: {request.message}')
 
-        packet_id: str = '%s' % request.message['_']
+        packet_id: str = f'{request.message.get("_", "__NOT_EXISTING_METHOD")}'
         if packet_id not in self.methods:
-            raise WrongConsumer(f'Callable method for {request.message} is not registered')
-
-        method_impl, packet_cls = self.methods[packet_id]
-        packet = packet_cls.load(request.message)
-
-        if check_is_async(method_impl):
-            result = await method_impl(self.app, packet)
+            if self.raise_on_unregistered:
+                raise WrongConsumer(f'Callable method for {request.message} is not registered')
         else:
-            result = method_impl(self.app, packet)
+            method_impl, packet_cls = self.methods[packet_id]
+            packet = packet_cls.load(request.message)
 
-        if isinstance(result, PacketWithID):
-            result = result.dump()
-        return result
+            if check_is_async(method_impl):
+                result = await method_impl(self.app, packet)
+            else:
+                result = method_impl(self.app, packet)
+
+            if isinstance(result, Packet):
+                result = result.dump()
+            return result
