f625f7ef967640fc7fb51ca2c16cefae1f0e7c1d
Vladimir Berezenko <qmaster2000@gmail.com>
2024-05-10 21:46:42 +0300

    Added possibility to post-decorate functions while registering an RPC

--- asyncframework/rpc/decorator.py
+++ asyncframework/rpc/decorator.py
@@ -9,11 +9,11 @@ __all__ = ['rpc_methods', 'rpc_method']
 rpc_methods: Dict[str, Tuple[Callable, Any]] = {}
 
 
-def rpc_method(methods: Dict[str, Tuple[Callable, Any]] = rpc_methods):
+def rpc_method(methods: Dict[str, Tuple[Callable, Any]] = rpc_methods, decorator: Callable[[Callable], Callable] | None = None):
     def wrapper(method: Callable):
         print(f'METHOD {method.__name__}')
         assert method.__name__ not in rpc_methods, f'Duplicate rpc method: {method.__name__}'
         set_if_async(method)
-        methods[method.__name__] = method, None
+        methods[method.__name__] = method if decorator is None else decorator(method), None
         return method
     return wrapper
