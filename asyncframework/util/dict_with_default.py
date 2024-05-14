45625c573d4b3ce8e6c9e3032aec916fa4006578
Vladimir Berezenko <qmaster2000@gmail.com>
2024-05-14 13:31:42 +0300

    Added template for dict with default value.

---
+++ asyncframework/util/dict_with_default.py
@@ -0,0 +1,13 @@
+# -*- coding:utf-8 -*-
+from typing import Dict, TypeVar
+
+
+_KT = TypeVar("_KT")
+_VT = TypeVar("_VT")
+
+
+class DictWithDefault(Dict[_KT, _VT]):
+    default: _VT | None = None
+
+    def __missing__(self, _) -> _VT:
+        return self.default
