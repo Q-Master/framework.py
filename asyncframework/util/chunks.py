1e5f4b0a3adbab4b7bbf81e2a6f0bec9660ab30d
Vladimir Berezenko <qmaster2000@gmail.com>
2025-02-11 20:11:59 +0300

    Fixed chunking.

--- asyncframework/util/chunks.py
+++ asyncframework/util/chunks.py
@@ -2,7 +2,7 @@
 from typing import TypeVar, Generator, List
 
 _T = TypeVar("_T")
-def chunks(l: int, n: List[_T]) -> Generator[List[_T], None, None]:
+def chunks(l: List[_T], n: int) -> Generator[List[_T], None, None]:
     """Yield successive n-sized chunks from l."""
     for i in range(0, len(l), n):
         yield l[i:i + n]
