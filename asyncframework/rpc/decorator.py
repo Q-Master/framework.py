51ee86486791fe3fbd48dff95476c9d1c7b02f79
Vladimir Berezenko <qmaster2000@gmail.com>
2025-09-07 01:51:10 +0300

    Added proper typing for RPC decorator

--- asyncframework/rpc/decorator.py
+++ asyncframework/rpc/decorator.py
@@ -1,5 +1,5 @@
 # -*- coding:utf-8 -*-
-from typing import Callable, Dict, Tuple, Any
+from typing import Callable, Dict, Tuple, Any, TypeVar
 from ..aio import set_if_async
 
 
@@ -8,9 +8,10 @@ __all__ = ['rpc_methods', 'rpc_method']
 
 rpc_methods: Dict[str, Tuple[Callable, Any]] = {}
 
+_FT = TypeVar('_FT', bound=Callable)
 
 def rpc_method(methods: Dict[str, Tuple[Callable, Any]] = rpc_methods, decorator: Callable[[Callable], Callable] | None = None):
-    def wrapper(method: Callable):
+    def wrapper(method: _FT) -> _FT:
         assert method.__name__ not in rpc_methods, f'Duplicate rpc method: {method.__name__}'
         set_if_async(method)
         methods[method.__name__] = method if decorator is None else decorator(method), None
--- asyncframework/rpc/packets/decorator.py
+++ asyncframework/rpc/packets/decorator.py
@@ -1,5 +1,5 @@
 # -*- coding:utf-8 -*-
-from typing import Type, Callable
+from typing import Type, Callable, TypeVar
 from packets import PacketBase
 from ...aio import set_if_async
 from ..decorator import rpc_methods
@@ -8,6 +8,8 @@ from ..decorator import rpc_methods
 __all__ = ['rpc_packet']
 
 
+_FT = TypeVar('_FT', bound=Callable)
+
 def rpc_packet(packet_cls: Type[PacketBase], methods = rpc_methods):
     """Decorator for adding methods to packet RPC
 
@@ -15,7 +17,7 @@ def rpc_packet(packet_cls: Type[PacketBase], methods = rpc_methods):
         packet_cls (Type[PacketBase]): packet type to use to serialize
         methods (Dict[str, Tuple[Callable, Any]], optional): array of mapping str: Callable. Defaults to rpc_methods.
     """
-    def decorator(method: Callable):
+    def decorator(method: _FT) -> _FT:
         set_if_async(method)
         if 'packet_id' in packet_cls.fields_names():
             methods[f'{packet_cls.packet_id.info.default}'] = method, packet_cls # type: ignore # if packet_id is in fields names it will be here
