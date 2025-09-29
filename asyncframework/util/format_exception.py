1cb24a814062ee7f9697db58f60213d3bf3e36df
Vladimir Berezenko <qmaster2000@gmail.com>
2025-09-29 22:12:11 +0300

    Fixed exception formatting. Added unpacking of exception str

--- asyncframework/util/format_exception.py
+++ asyncframework/util/format_exception.py
@@ -2,6 +2,9 @@
 import traceback
 
 
-def format_exception(exc: Exception, delimiter='#012'):
-    exc_strs = traceback.format_exception(exc)
-    return delimiter.join(exc_strs)
+def format_exception(exc: Exception, delimiter='#012') -> str:
+    exc_strs = [estr.replace('\n', delimiter) for estr in traceback.format_exception(exc)]
+    return ''.join(exc_strs)
+
+def unpack_exception_str(exc: str, delimiter='#012') -> str:
+    return exc.replace(delimiter, '\n')
